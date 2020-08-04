90782aba671e5de04430b98d5c58f76e27404cb4
U-DESKTOP-G6QH3VR\sxy <shengxiaoyi2014@icloud.com>
2020-08-04 16:56:51 +0800

    modify exception

--- demo/exceptions/src/main.cpp
+++ demo/exceptions/src/main.cpp
@@ -1,13 +1,57 @@
-#include <iostream>
-#include "export.h"
+// #include <iostream>
+// #include "export.h"
+//
+// using namespace std;
+//  int main() {
+//    try{
+//      test_overflow_error();
+//    }
+//    catch(...){
+//      std::cout << "argument catched" << '\n';
+//    }
+//    return 0;
+// }
 
+
+#include <iostream>
+#include <stdlib.h>
+#include <string>
 using namespace std;
- int main() {
-   try{
-     test_overflow_error();
-   }
-   catch(...){
-     std::cout << "argument catched" << '\n';
-   }
-   return 0;
+
+class MyErr{
+public:
+	MyErr(string errmsg){
+		cout << "构造异常类" << endl;
+		this->errmsg=errmsg;
+	}
+	MyErr(const MyErr& err){
+		cout << "拷贝构造异常类" << endl;
+		this->errmsg=err.errmsg;
+	}
+	MyErr& operator=(const MyErr& err){
+		cout << "拷贝赋值异常类" << endl;
+		this->errmsg=err.errmsg;
+		return *this;
+	}
+	~MyErr(){
+		cout << "析构异常类" << endl;
+	}
+
+public:
+	string errmsg;
+};
+
+void func1(){
+	int a=10;//当抛出异常会被销毁
+
+	throw MyErr("抛出异常");
+}
+int main(){
+
+	try{
+		func1();
+	}catch(MyErr e){
+		cout << "捕获异常" << endl;
+	}
+	system("pause");
 }
